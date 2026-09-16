f2bbf32c6aabfaac018c4db15bdcea374aa8b0bf
MahderK <mahderkassaw@gmail.com>
2026-09-16 14:04:22 +0300

    Add desktop icons and refactor
    - Added desktop icons for contact
    - refactor old unused code

--- client/src/components/Desktop/Desktop.jsx
+++ client/src/components/Desktop/Desktop.jsx
@@ -8,7 +8,7 @@ import ContactView from "../content/ContactView";
 const apps = [
   { id: "projects", label: "Projects", icon: "#documentation-icon", component: ProjectsView },
   { id: "skills", label: "Skills", icon: "#social-icon", component: SkillsView },
-  { id: "contact", label: "Contact", icon: "#github-icon", component: ContactView },
+  { id: "contact", label: "Contact", icon: `${import.meta.env.BASE_URL}icons8-github-64.png`, component: ContactView },
 ];
 
 function Desktop() {
--- client/src/components/Desktop/DesktopIcon.jsx
+++ client/src/components/Desktop/DesktopIcon.jsx
@@ -1,20 +1,7 @@
-import { useState, useEffect } from "react";
-import ProjectsView from "../content/ProjectsView";
-import SkillsView from "../content/SkillsView";
-import ContactView from "../content/ContactView";
-
-const componentMap = {
-  ProjectsView,
-  SkillsView,
-  ContactView,
-};
-
 function DesktopIcon({ app, onOpen }) {
   return (
     <button className="desktop-icon" onDoubleClick={() => onOpen(app)}>
-      <svg className="desktop-icon-img">
-        <use href={app.icon} />
-      </svg>
+      <img className="desktop-icon-img" src={app.icon} alt={app.label} />
       <span className="desktop-icon-label">{app.label}</span>
     </button>
   )
